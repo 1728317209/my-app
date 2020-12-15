ddd8fa5d9af7e397886320924425310217c01883
杨帅 <yangshuai@xiaoniangao.com>
2020-12-15 10:46:09 +0800

    feat: init

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,12 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+
+test('a and b', () => {
+  const a = 1;
+  const b = 1;
+  expect(a + b).toBe(2);
+});
 
 test('renders learn react link', () => {
   render(<App />);
